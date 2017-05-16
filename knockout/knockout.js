df2c18148971d24f329e13bf522f22aeada1f476
Elliot Rowe <gouzusama@gmail.com>
2017-05-16 10:38:01 -0700

    Implementing changes from meeting
    Made discussed changes to knockout and the ionRangeSlider css file

--- knockout/knockout.js
+++ knockout/knockout.js
@@ -37,23 +37,22 @@ var tooltip_style = {
  'box-shadow': '4px 6px 20px 0px rgba(0, 0, 0, 0.4)',
 }
 
-function initialize_knockout() {
+function initialize_knockout () {
     // load everything
     load_builder(function(builder) {
         load_model(function(model) {
             var old_model = escher.utils.clone(model);
             optimize_loop(builder, model);
-            d3.select('#reset-button')
-                .on('click', function() {
-                    model = escher.utils.clone(old_model);
-                    optimize_loop(builder, model);
+            d3.select('#reset-button').on('click', function () {
+                    model = escher.utils.clone(old_model)
+                    optimize_loop(builder, model)
                 });
         });
     });
 }
 
 
-function load_builder(callback) {
+function load_builder (callback) {
     // load the Builder
     d3.json('E coli core.Core metabolism.json', function(e, data) {
         if (e) console.warn(e);
@@ -64,56 +63,37 @@ function load_builder(callback) {
                         reaction_styles: ['abs', 'color', 'size', 'text'],
                         never_ask_before_quit: true,
                         tooltip_component: tooltips_1,
-                        enable_tooltips: true };
-        var b = escher.Builder(data, null, null, d3.select('#map_container'), options);
-        callback(b);
+                        enable_tooltips: true, }
+        var b = escher.Builder(data, null, null, d3.select('#map_container'), options)
+        callback(b)
     });
 }
 
 
 function load_model(callback) {
-    d3.json('E coli core.json', function(e, data) {
-        if (e) console.warn(e);
-        callback(data);
+    d3.json('E coli core.json', function (e, data) {
+        if (e) console.warn(e)
+        callback(data)
     });
 }
 
 
-function set_knockout_status(text) {
-    d3.select('#knockout-status').text(text);
+function set_knockout_status (text) {
+    d3.select('#knockout-status').text(text)
 }
 
 
-function optimize_loop(builder, model) {
-    var solve_and_display = function(m, knockouts) {
-        var problem = build_glpk_problem(m);
-        var result = optimize(problem);
-        var keys = Object.keys(knockouts),
-            ko_string = keys.map(function(s) { return 'Δ'+s; }).join(' ');
-        var nbs = String.fromCharCode(160); // non-breaking space
-        if (keys.length > 0)
-            ko_string += (' (' + keys.length + 'KO): ');
-        else ko_string = 'Click a reaction to knock it out. ';
-        if (result.f < 1e-3) {
-            builder.set_reaction_data(null);
-            set_knockout_status(ko_string + 'You killed E.' + nbs + 'coli!');
-        } else {
-            builder.set_reaction_data(result.x);
-            set_knockout_status(ko_string + 'Growth' + nbs + 'rate:' + nbs +
-                                (result.f/1.791*100).toFixed(1) + '%');
-        }
-    };
-
-    var knockouts = {},
-        knockable = function(r) {
-            return (r.indexOf('EX_') == -1 &&
-                    r.indexOf('ATPM') == -1 &&
-                    r.indexOf('Biomass') == -1);
-        };
+function optimize_loop (builder, model) {
+    var knockouts = {}
+    var knockable = function(r) {
+      return (r.indexOf('EX_') == -1
+              && r.indexOf('ATPM') == -1
+              && r.indexOf('Biomass') == -1)
+    }
 
     // set up and run
-    model = set_carbon_source(model, 'EX_glc_e', 20);
-    solve_and_display(model, knockouts);
+    model = set_carbon_source(model, 'EX_glc_e', 20)
+    solve_and_display(model, builder, knockouts)
 
     // initialize event listeners
     var sel = builder.selection;
@@ -127,7 +107,7 @@ function optimize_loop(builder, model) {
                 if (!(d.bigg_id in knockouts))
                     knockouts[d.bigg_id] = true;
                 model = knock_out_reaction(model, d.bigg_id);
-                solve_and_display(model, knockouts);
+                solve_and_display(model, builder, knockouts);
             }
         });
     // grey for reactions that cannot be knocked out
@@ -136,17 +116,78 @@ function optimize_loop(builder, model) {
             if (!knockable(d.bigg_id)) return '#888';
             else return null;
         });
+
+    builder.options.tooltip_component = function (args) {
+      // Check if there is already text in the tooltip
+      if (args.el.childNodes.length === 0) {
+        // If not, add new text
+        // var node = document.createTextNode('Hello ')
+        // var br = document.createElement('br')
+        var $input = $('<input>').appendTo(args.el)
+        $input.ionRangeSlider({
+          hide_min_max: true,
+          keyboard: true,
+          min: -1000,
+          max: 1000,
+          from: -1000,
+          to: 1000,
+          type: 'double',
+          step: 1,
+          grid: true,
+        })
+      // Style the text based on our tooltip_style object
+        Object.keys(tooltip_style).map(function (key) {
+          args.el.style[key] = tooltip_style[key]
+        })
+      }
+      var $input = $(args.el).children('input')
+      var slider_data = $input.data("ionRangeSlider")
+      slider_data.update({
+        onFinish: function (data) {
+          console.log(args.state.biggId)
+          // for (var i = 0, l = model.reactions.length; i < l; i++) {
+          //   if (model.reactions[i].id == reaction_id) {
+          //       console.log(model.reaction[i].lower_bound)
+          //   }
+          // }
+        }
+      })
+      slider_data.reset()
+    // Update the text to read out the identifier biggId
+
+    //args.el.childNodes[0].textContent = 'Hello ' + args.state.biggId;
+    }
+    builder.load_map(builder.map_data)
+}
+
+solve_and_display = function (model, builder, knockouts) {
+    var problem = build_glpk_problem(model)
+    var result = optimize(problem)
+    var keys = Object.keys(knockouts)
+    var ko_string = keys.map(function (s) { return 'Δ'+s; }).join(' ');
+    var nbs = String.fromCharCode(160); // non-breaking space
+    if (keys.length > 0)
+        ko_string += (' (' + keys.length + 'KO): ');
+    else ko_string = 'Click a reaction to knock it out. ';
+    if (result.f < 1e-3) {
+        builder.set_reaction_data(null);
+        set_knockout_status(ko_string + 'You killed E.' + nbs + 'coli!');
+    } else {
+        builder.set_reaction_data(result.x);
+        set_knockout_status(ko_string + 'Growth' + nbs + 'rate:' + nbs +
+                            (result.f/1.791*100).toFixed(1) + '%');
+    }
 }
 
 
-function fill_array(len, val) {
+function fill_array (len, val) {
     for (var i = 0, arr = new Array(len); i < len;)
         arr[i++] = val;
     return arr;
 }
 
 
-function fill_array_single(len, val, index_value, index) {
+function fill_array_single (len, val, index_value, index) {
     for (var i = 0, arr = new Array(len); i < len;) {
         if (i == index)
             arr[i++] = index_value;
@@ -157,42 +198,42 @@ function fill_array_single(len, val, index_value, index) {
 }
 
 
-function knock_out_reaction(model, reaction_id) {
+function knock_out_reaction (model, reaction_id) {
     for (var i = 0, l = model.reactions.length; i < l; i++) {
         if (model.reactions[i].id == reaction_id) {
-            model.reactions[i].lower_bound = 0.0;
-            model.reactions[i].upper_bound = 0.0;
+            model.reactions[i].lower_bound = 0.0
+            model.reactions[i].upper_bound = 0.0
             return model;
         }
     }
-    throw new Error('Bad reaction ' + reaction_id);
+    throw new Error('Bad reaction ' + reaction_id)
 }
 
 
-function change_flux_reaction(model, reaction_id, lower, upper) {
+function change_flux_reaction (model, reaction_id, lower, upper) {
     for (var i = 0, l = model.reactions.length; i < l; i++) {
         if (model.reactions[i].id == reaction_id) {
-            model.reactions[i].lower_bound = lower;
-            model.reactions[i].upper_bound = upper;
+            model.reactions[i].lower_bound = lower
+            model.reactions[i].upper_bound = upper
             return model;
         }
     }
-    throw new Error('Bad reaction ' + reaction_id);
+    throw new Error('Bad reaction ' + reaction_id)
 }
 
 
 function set_carbon_source(model, reaction_id, sur) {
     for (var i = 0, l = model.reactions.length; i < l; i++) {
         if (model.reactions[i].id == reaction_id) {
-            model.reactions[i].lower_bound = -sur;
-            return model;
+            model.reactions[i].lower_bound = -sur
+            return model
         }
     }
-    throw new Error('Bad carbon source ' + reaction_id);
+    throw new Error ('Bad carbon source ' + reaction_id)
 }
 
 
-function build_glpk_problem(model) {
+function build_glpk_problem (model) {
     /** Build a GLPK LP for the model.
 
      Arguments
@@ -207,26 +248,26 @@ function build_glpk_problem(model) {
         met_lookup = {};
 
     // initialize LP objective
-    var lp = glp_create_prob();
-    glp_set_prob_name(lp, 'knockout FBA');
+    var lp = glp_create_prob ();
+    glp_set_prob_name (lp, 'knockout FBA');
     // maximize
-    glp_set_obj_dir(lp, GLP_MAX);
+    glp_set_obj_dir (lp, GLP_MAX);
     // set up rows and columns
-    glp_add_rows(lp, n_rows);
-    glp_add_cols(lp, n_cols);
+    glp_add_rows (lp, n_rows);
+    glp_add_cols (lp, n_cols);
 
     // metabolites
-    model.metabolites.forEach(function(metabolite, i) {
+    model.metabolites.forEach (function (metabolite, i) {
         var row_ind = i + 1;
-        glp_set_row_name(lp, row_ind, metabolite.id);
-        glp_set_row_bnds(lp, row_ind, GLP_FX, 0.0, 0.0);
+        glp_set_row_name (lp, row_ind, metabolite.id);
+        glp_set_row_bnds (lp, row_ind, GLP_FX, 0.0, 0.0);
         // remember the indices of the metabolites
         met_lookup[metabolite.id] = row_ind;
     });
 
     // reactions
     var mat_ind = 1;
-    model.reactions.forEach(function(reaction, i) {
+    model.reactions.forEach (function (reaction, i) {
         var col_ind = i + 1;
 
         glp_set_col_name(lp, col_ind, reaction.id);
@@ -251,11 +292,11 @@ function build_glpk_problem(model) {
 }
 
 
-function optimize(problem) {
+function optimize (problem) {
     var smcp = new SMCP({presolve: GLP_ON});
-    glp_simplex(problem, smcp);
+    glp_simplex (problem, smcp);
     // get the objective
-    var f = glp_get_obj_val(problem);
+    var f = glp_get_obj_val (problem);
     // get the primal
     var x = {};
     for (var i = 1; i <= glp_get_num_cols(problem); i++){
@@ -270,56 +311,41 @@ var tooltips_1 = function (args) {
     // If not, add new text
     // var node = document.createTextNode('Hello ')
     // var br = document.createElement('br')
-    var $input = $('<input>').appendTo(args.el);
+    var $input = $('<input>').appendTo(args.el)
     $input.ionRangeSlider({
-        hide_min_max: true,
-        keyboard: true,
-        min: -1000,
-        max: 1000,
-        from: -1000,
-        to: 1000,
-        type: 'double',
-        step: 1,
-        grid: true,
-        onFinish: function(data) {
-          console.log(args.state.biggId);
-          console.log(args.state.lower_bound);
-          // if (knockable(d.bigg_id)) {
-          //     if (!(d.bigg_id in knockouts))
-          //         knockouts[d.bigg_id] = true;
-          //     model = change_flux_reaction(model, d.bigg_id, data.from, data.to);
-          //     solve_and_display(model, knockouts);
-          // }
-        }
-    });
-    // Style the text based on our tooltip_style object
+      hide_min_max: true,
+      keyboard: true,
+      min: -1000,
+      max: 1000,
+      from: -1000,
+      to: 1000,
+      type: 'double',
+      step: 1,
+      grid: true,
+    })
+  // Style the text based on our tooltip_style object
     Object.keys(tooltip_style).map(function (key) {
       args.el.style[key] = tooltip_style[key]
     })
-  };
-  var $input = $(args.el).children('input');
-  var slider_data = $input.data("ionRangeSlider");
-  slider_data.reset();
-  // Update the text to read out the identifier biggId
+  }
+  var $input = $(args.el).children('input')
+  var slider_data = $input.data("ionRangeSlider")
+  slider_data.update({
+    onFinish: function (data) {
+      console.log(args.state.biggId)
+      // for (var i = 0, l = model.reactions.length; i < l; i++) {
+      //   if (model.reactions[i].id == reaction_id) {
+      //       console.log(model.reaction[i].lower_bound)
+      //   }
+      // }
+    }
+  })
+  slider_data.reset()
+// Update the text to read out the identifier biggId
 
-  //args.el.childNodes[0].textContent = 'Hello ' + args.state.biggId;
+//args.el.childNodes[0].textContent = 'Hello ' + args.state.biggId;
 }
 
 window.onload = function () {
-
-  // d3.json('E coli core.Core metabolism.json', function(e, data) {
-  //   if (e) console.warn(e)
-  //   var options = {
-  //     menu: 'zoom',
-  //     fill_screen: true,
-  //     // --------------------------------------------------
-  //     // CHANGE ME
-  //     tooltip_component: tooltips_1,
-  //     // --------------------------------------------------
-  //   }
-  //   var b = escher.Builder(data, null, null, d3.select('#map_container'),
-  //                          options)
-  // })
-  initialize_knockout();
-
+  initialize_knockout()
 }
